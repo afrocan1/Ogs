7f4feebb4a98424231ddf0ccd9429cbea4def1a1
afrocan1 <114925535+afrocan1@users.noreply.github.com>
2026-07-14 13:51:22 +0000

    Update Integrationscolumn.tsx

--- src/components/Integrationscolumn.tsx
+++ src/components/Integrationscolumn.tsx
@@ -36,7 +36,7 @@ export default function Integrationscolumn( props:{
                                     {integrations.map(integration=>(
                             <div key={integration.name} className="bg-neutral-900 border border-white/10 rounded-3xl p-6">
                                 <div className="flex justify-center">
-                                    <Image src={integration.icon} alt={integration.name} className="size-24" />
+                                    <integration.icon className="size-12 mx-auto text-yellow-400" strokeWidth={1.5} />
 
                                 </div>
                                 <h3 className="text-3xl text-center mt-6">{integration.name}</h3>
@@ -50,4 +50,3 @@ export default function Integrationscolumn( props:{
     </motion.div>
   )
 }
-
