0cb47cba8a5b63d9e7f03a146ff3dcb125384512
afrocan1 <114925535+afrocan1@users.noreply.github.com>
2026-07-14 14:49:19 +0000

    Update LogoTicker.tsx

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -4,14 +4,9 @@ import React, { Fragment } from "react";
 import { motion } from "framer-motion";
 
 const logos = [
-  { name: "Quantum", image: "/images/quantum.svg" },
-  { name: "Acme Corp", image: "/images/acme-corp.svg" },
-  { name: "Echo Valley", image: "/images/echo-valley.svg" },
-  { name: "Pulse", image: "/images/pulse.svg" },
-  { name: "Outside", image: "/images/outside.svg" },
-  { name: "Apex", image: "/images/apex.svg" },
-  { name: "Celestial", image: "/images/celestial.svg" },
-  { name: "Twice", image: "/images/twice.svg" },
+  { name: "Arbitrum", image: "/images/arb-removebg-preview.png" },
+  { name: "BNB", image: "/images/bnb-removebg-preview.png" },
+  { name: "GOA", image: "/images/goa-removebg-preview.png" },
 ];
 
 
@@ -21,7 +16,7 @@ export default function LogoTicker() {
       <div className="container">
       
         <h3 className="text-center text-white/50 text-xl">
-          Already chosen by these market leaders
+          Checking balances across
         </h3>
         <div className=" flex overflow-hidden mt-12 [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <motion.div
