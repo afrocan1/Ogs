6d28745f25195e7fb1d61a52b9dd334e9a64b959
afrocan1 <114925535+afrocan1@users.noreply.github.com>
2026-07-14 15:18:10 +0000

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,8 +1,5 @@
 "use client"
 import Button from "@/components/Button";
-import designExample1Image from "@/assets/images/design-example-1.png"
-import designExample2Image from "@/assets/images/design-example-2.png"
-import Image from "next/image";
 import Pointer from "@/components/Pointer";
 import {motion,useAnimate } from "framer-motion"
 import { useEffect } from "react";
@@ -11,27 +8,15 @@ import cursorYouImage from "@/assets/images/cursor-you.svg";
 
 
 export default function Hero() {
-    const [leftDesignScope,leftDesignAnimate]=useAnimate();
     const [leftPointerScope,leftPointerAnimate]=useAnimate();
-    const[rightDesignScope,rightDesignAnimate]=useAnimate();
     const[rightPointerScope,rightPointerAnimate]=useAnimate();
 
     useEffect(()=>{
-        leftDesignAnimate([
-            [leftDesignScope.current, {opacity:[0,1]},{duration:0.5}],
-            [leftDesignScope.current, {y:0,x:0},{duration:0.5}]
-        ])
-
         leftPointerAnimate([
             [leftPointerScope.current,{opacity:1},{duration:0.5}],
             [leftPointerScope.current,{y:0,x:-100},{duration:0.5}],
             [leftPointerScope.current,{x:0, y:[0,16,0] },{duration:0.5,ease:"easeInOut"}] 
         ]);
-
-        rightDesignAnimate([ 
-            [rightDesignScope.current,{opacity:1},{duration:0.5,delay:1.5}],
-            [rightDesignScope.current,{y:0,x:0},{duration:0.5}]
-        ])
 
         rightPointerAnimate([
             [rightPointerScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
@@ -43,31 +28,16 @@ export default function Hero() {
     },[  leftPointerAnimate,
         rightPointerAnimate,
         leftPointerScope,
-        rightPointerScope,
-        leftDesignAnimate,
-        leftDesignScope,
-        rightDesignAnimate,
-        rightDesignScope,])
+        rightPointerScope,])
 
     return <section className="py-24 overflow-x-clip " style={{ 
         cursor:`url(${cursorYouImage.src}),auto`
      }}>
         <div className="container relative">
-            <motion.div ref={leftDesignScope} initial={{opacity:1,y:100,x:-100 }} drag className="absolute -left-32 top-16 hidden lg:block">
-                <Image 
-                    src={designExample1Image} 
-                    alt="Design example image 1"
-                    draggable="false" 
-                />
-            </motion.div>
             <motion.div ref={leftPointerScope}  initial={{opacity:0, y:100,x:-200}}  className="absolute left-56 top-96 hidden lg:block">
                       <Pointer name="OG" color="blue" />
             </motion.div>
             {/* {Right side animation} */}
-            <motion.div ref={rightDesignScope} initial={{opacity:0,x:100,y:100}} drag className="absolute -right-64 -top-16 hidden lg:block  ">
-                <Image src={designExample2Image} draggable="false" alt="Design example image 2"/>
-            </motion.div>
-         
             <motion.div 
             ref={rightPointerScope}
             initial={{opacity:0,x:275,y:100}}
